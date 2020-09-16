4f4211b7d5093b50c335a134a831b3d7472e4194
Sy Rashid <sy.rashid.11@gmail.com>
2020-09-16 20:53:39 +0300

    FEAT: Set up devise for a customer username field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   add_flash_types :info, :success
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
