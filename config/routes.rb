138eed367af316ac4bf553bdd0ff4f625ad323e2
Sy Rashid <sy.rashid.11@gmail.com>
2020-09-19 17:43:22 +0300

    FEAT: Added seed file with users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'groups/index'
   devise_for :users
   get '/kitchensink', to: 'pages#kitchensink' if Rails.env.development?
+
   root to: 'groups#index'
 
   resources :groups, only: :index
