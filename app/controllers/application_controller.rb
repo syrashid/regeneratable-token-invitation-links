6204707deddbcf6355c5a24d7c5aa3b7620a0204
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-01 07:43:23 +0300

    FEAT: Styling for toast

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :invite_token])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:invite_token])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
