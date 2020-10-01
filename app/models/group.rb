fe71b17b0d0e1d275de8170c5a7a3040297c4c6d
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-01 11:18:55 +0300

    FIX: Dependent destroy for memberships on groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   has_secure_token :invite_token
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
 
   def member?(user)
