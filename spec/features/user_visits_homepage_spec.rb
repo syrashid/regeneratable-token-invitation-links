1bc8a1af42377eba8ff8cd5b4634d92954aefd9f
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-28 17:33:36 +0300

    FEAT: Added secondary test

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -8,6 +8,21 @@ feature "Users visits root..." do
   end
 
   scenario "and sees personal dashboard after logging in" do
+    # Set Up
+    User.create!(username: 'maya_nicolas', email: 'gracia@baumbach.com', password: 'password')
 
+    # Exercise
+    visit root_path
+
+    fill_in "Email", with: "gracia@baumbach.com"
+    fill_in "Password", with: "password"
+
+    click_button("commit")
+
+    # Verify
+    expect(page).to have_css('h3', text: 'maya_nicolas Dashboard')
+
+    # Teardown
+    User.destroy_all
   end
 end
