3245848cbccf768af051334ff5a3840374c90f20
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-31 20:22:12 +0300

    FEAT: Seperated acceptance test into two

--- spec/features/user_copies_link_spec.rb
+++ spec/features/user_copies_link_spec.rb
@@ -1,27 +1,17 @@
 require 'rails_helper'
 
-feature "Users regenerates and copies invite link..." do
+feature "Users copies invite link" do
   scenario "successfully" do
-    # Set Up
     sam = User.create!(username: 'sam_gamgee', email: 'sam@hobbit.com', password: 'password')
     mordor = Group.create(name: "Mordor", topic: "There's some good in this world, Mr. Frodo, and it's worth fighting for.")
     Membership.create(user: sam, group: mordor)
 
-    # Exercise
-    ## Log In
-
     sign_in_as(sam)
-
-    ## Regenerate
     click_on("Mordor")
-    old_invite_link = find_field("cb-link-field").value
-    click_button("regenerate-btn")
-
-    # Verify
+    click_button("Copy Link")
 
     expect(old_invite_link).not_to eq(find_field("cb-link-field").value)
 
-    # Teardown
     Membership.destroy_all
     Group.destroy_all
     User.destroy_all
---
+++ spec/features/user_regenerates_link_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature "Users regenerates invite link" do
+  scenario "successfully" do
+    # Set Up
+    sam = User.create!(username: 'sam_gamgee', email: 'sam@hobbit.com', password: 'password')
+    mordor = Group.create(name: "Mordor", topic: "There's some good in this world, Mr. Frodo, and it's worth fighting for.")
+    Membership.create(user: sam, group: mordor)
+
+    # Exercise
+    ## Log In
+
+    sign_in_as(sam)
+
+    ## Regenerate
+    click_on("Mordor")
+    old_invite_link = find_field("cb-link-field").value
+    click_button("regenerate-btn")
+
+    # Verify
+
+    expect(old_invite_link).not_to eq(find_field("cb-link-field").value)
+
+    # Teardown
+    Membership.destroy_all
+    Group.destroy_all
+    User.destroy_all
+  end
+end
