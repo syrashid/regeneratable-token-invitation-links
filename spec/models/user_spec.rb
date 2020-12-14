d0d5b88965981c3242ce8ba624bd73ec98bb0593
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-14 18:03:39 +0100

    FEAT: Wrote out username present validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validation tests' do
-    it 'ensures a username is present' do
-
+    it 'a user is valid with a username present' do
+      valid_user = build(:user)
+      expect(valid_user).to be_valid
+    end
+    it 'a user is invalid with a username missing' do
+      invalid_user = build(:user, username: nil)
+      expect(invalid_user).to_not be_valid
     end
     it 'ensures a username is unique' do
 
