9221335d4c5d4b3fada35994c3689af2549e5893
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-14 17:42:00 +0100

    FEAT: Set up base arch for user validation tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'validation tests' do
+    it 'ensures a username is present' do
+
+    end
+    it 'ensures a username is unique' do
+
+    end
+  end
+end
