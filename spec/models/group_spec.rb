ce1562d3f9571fc8011c63004454cd6baf1169b0
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-14 18:30:13 +0100

    FEAT: Wrote tests for has_secure_token in groups

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'association tests' do
+    it { should have_many(:memberships) }
+    it { should have_many(:users) }
+  end
+
+  describe 'invite token methods (has_secure_token)' do
+    it 'a group has a unique token generated upon creation' do
+      group = create(:group)
+
+      expect(group.invite_token).not_to be_nil
+    end
+
+    it 'a group generates a new token upon invocation' do
+      group = create(:group)
+
+      old_token = group.invite_token
+      group.regenerate_invite_token
+
+      expect(group.invite_token).not_to eq(old_token)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,15 +4,18 @@ RSpec.describe User, type: :model do
   describe 'validation tests written vanilla' do
     it 'a user is valid with a username present' do
       valid_user = build(:user)
+
       expect(valid_user).to be_valid
     end
     it 'a user is invalid with a username missing' do
       invalid_user = build(:user, username: nil)
+
       expect(invalid_user).to_not be_valid
     end
     it 'ensures a username is unique' do
       valid_user = create(:user, username: 'sample_name')
       invalid_user = build(:user, username: 'sample_name')
+
       expect(invalid_user).to_not be_valid
     end
   end
