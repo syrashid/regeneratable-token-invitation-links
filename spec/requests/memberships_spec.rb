dbedfe039441c5cac9a96e45cb633567c69f8646
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-15 19:04:18 +0100

    FEAT: Wrote post membership create test

--- spec/requests/memberships_spec.rb
+++ spec/requests/memberships_spec.rb
@@ -16,6 +16,17 @@ RSpec.describe "Memberships", type: :request do
 
   describe "POST memberships#create" do
     it "should create a membership if none exists" do
+      group = create(:group)
+      user = create(:user)
+      sign_in user
+
+      post group_memberships_path(group)
+
+      expect(Membership.count).to eq(1)
+      expect(response).to have_http_status(302)
+    end
+
+    it "should not create a membership if it already exists" do
 
     end
   end
