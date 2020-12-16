1187a5140015d4160a6de7db6533817c1d807a27
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-16 22:24:46 +0100

    TEST: Finished final copy test

--- spec/features/user_copies_link_spec.rb
+++ spec/features/user_copies_link_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
 feature "Users copies invite link..." do
-  xscenario "successfully" do
-    sam = User.create!(username: 'sam_gamgee', email: 'sam@hobbit.com', password: 'password')
-    mordor = Group.create(name: "Mordor", topic: "There's some good in this world, Mr. Frodo, and it's worth fighting for.")
-    Membership.create(user: sam, group: mordor)
+  scenario "successfully", js: true do
+    user = create(:user)
+    group = create(:group)
+    membership = create(:membership, user: user, group: group)
+    sign_in user
 
-    sign_in_as(sam)
-    click_on("Mordor")
+    visit group_path(group)
+    click_button('groupname-btn')
     click_button("Copy Link")
 
-    expect(old_invite_link).not_to eq(find_field("cb-link-field").value)
-
-    Membership.destroy_all
-    Group.destroy_all
-    User.destroy_all
+    expect(page).to have_content('Link copied to clipboard!')
   end
 end
