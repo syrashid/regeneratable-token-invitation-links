ec10e6c7fb158f7a594671a3e718f97c9915e84a
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-16 20:48:12 +0100

    TEST: Fixed js issue with capybara and stimulus due to db cleaner

--- spec/features/user_regenerates_link_spec.rb
+++ spec/features/user_regenerates_link_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 feature "Users regenerates invite link..." do
-  xscenario "successfully", js: true do
+  scenario "successfully", js: true do
     user = create(:user)
     group = create(:group)
     membership = create(:membership, user: user, group: group)
     sign_in user
 
     visit group_path(group)
-
-    old_invite_token = group.invite_token
+    click_button('groupname-btn')
+    old_invite_token = find('#cb-link-field')['value']
     click_button("regenerate-btn")
 
-    expect(old_invite_token).not_to eq(group.invite_token)
+    expect(old_invite_token).not_to eq(find('#cb-link-field')['value'])
   end
 end
